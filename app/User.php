88d39aa6213a89aaf03039565f5f40c9207e8474
AlexandrIgn <asignatyev@mail.ru>
2019-12-16 23:26:18 +0400

    add field last_name to Users table

--- app/User.php
+++ app/User.php
@@ -20,7 +20,6 @@ class User extends Authenticatable
         'name', 'last_name', 'email', 'password', 'status', 'role',
     ];
 
-
     protected $hidden = [
         'password', 'remember_token'
     ];
